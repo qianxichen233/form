9a8f7244239935e53c8a58368a456cb6f5a39b4d
qianxichen <qc815@nyu.edu>
2022-11-13 01:21:49 -0500

    added a backend node to receive questionnaire info

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const cors = require('cors');
 const http = require('http');
 const helmet = require('helmet');
 const express = require('express');
+const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
 
 require('dotenv').config();
@@ -11,9 +13,19 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Get request!');
+});
+
+app.post('/questionnaire', (req, res) => {
+	console.log(req.body);
+	res.json({
+		msg: 'Success'
+	});
 })
 
 http.createServer(app)
