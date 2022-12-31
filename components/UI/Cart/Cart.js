ab194402c70f89de8c5c23c264c171b4df9b0213
Qianxi Chen <qc815@nyu.edu>
2022-12-30 22:02:37 -0500

    fixed cart scroll to top

--- components/UI/Cart/Cart.js
+++ components/UI/Cart/Cart.js
@@ -1,36 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-import classes from './Cart.module.css';
+import classes from "./Cart.module.css";
 
 const Cart = React.forwardRef((props, cartRef) => {
-    const className = `${classes.cart} ${props.Error ? classes.error : ''} ${props.className ? props.className : ''}`;
-    const borderClass = `${classes.borderLeft} ${props.Focus ? classes.focus : ''}`;
+    const className = `${classes.cart} ${props.Error ? classes.error : ""} ${
+        props.className ? props.className : ""
+    }`;
+    const borderClass = `${classes.borderLeft} ${
+        props.Focus ? classes.focus : ""
+    }`;
 
     const ref = useRef(null);
 
     useEffect(() => {
-        if(props.ScrollTo && ref.current)
-        {
-            ref.current.scrollIntoView();
+        if (props.ScrollTo && ref.current) {
+            //ref.current.scrollIntoView();
+            window.scrollTo({
+                behavior: "smooth",
+                top:
+                    ref.current.getBoundingClientRect().top -
+                    document.body.getBoundingClientRect().top -
+                    110,
+            });
             props.cancelScroll();
         }
     }, [props.ScrollTo, ref.current]);
 
     useEffect(() => {
-        if(typeof(cartRef) == 'function') cartRef(ref.current);
-        else if(cartRef)
-            cartRef.current = ref.current;
+        if (typeof cartRef == "function") cartRef(ref.current);
+        else if (cartRef) cartRef.current = ref.current;
     }, [ref.current]);
 
-    return <div
-        className={className}
-        onClick={props.onClick}
-        id={props.id}
-        ref={ref}
-    >
-        <div className={borderClass}></div>
-        {props.children}
-    </div>
+    return (
+        <div
+            className={className}
+            onClick={props.onClick}
+            id={props.id}
+            ref={ref}
+        >
+            <div className={borderClass}></div>
+            {props.children}
+        </div>
+    );
 });
 
 export default Cart;
